434051b9cd2dbeb698cfba590277a976a776dcf8
Chris D. MacRae <hello@chrisdmacrae.com>
2021-02-10 20:13:06 -0400

    Initial plugin

--- src/fields/Conditional.tsx
+++ src/fields/Conditional.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react'
+import React, { useMemo } from 'react'
 import { Field, FieldPlugin, FieldsBuilder } from 'tinacms'
 import { getParentField } from '../getParentField'
 
